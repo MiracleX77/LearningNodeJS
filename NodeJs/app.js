04b4c3c1d9ef579f818f67131bae7e4905acf100
Phatchara_Mix <99187452+MiracleX77@users.noreply.github.com>
2023-05-30 16:10:24 +0700

    .

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app  = express()
 const path = require('path')
 const router = require('./routes/router')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
 
 app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+app.use(session({
+    secret:"mysession",
+    resave:false,
+    saveUninitialized:false
+}))
 app.use(router)
 app.use(express.static(path.join(__dirname,'public')))
 
--- NodeJs/routes/router.js
+++ NodeJs/routes/router.js
@@ -2,32 +2,88 @@ const express = require('express')
 const router = express.Router()
 //เรียกใช้งานโมเดล
 const Product = require('../models/products')
+//อัพโหลดไฟล์
+const multer = require('multer')
 
-
-router.get('/',(req,res)=>{
-    const products = [
-        {name:"notebook",price:25500,images:"images/products/product1.png"},
-        {name:"cloth",price:2000,images:"images/products/product2.png"},
-        {name:"headgear",price:30000,images:"images/products/product3.png"},
-
-    ]
-    res.render('index',{products:products})
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./public/images/products')//ตำเเหน่งจัดเก็บไฟล์
+    },
+    filename:function(req,file,cb){
+        cb(null,Date.now()+".jpg")//ป้องกันชื่อไฟลฒซ้ำ
+    }
 })
 
-router.get('/addform',(req,res)=>{
-    res.render('form')
+const upload = multer({
+    storage:storage
 })
 
-router.get('/manage',(req,res)=>{
-    res.render('manage')
+router.get('/',async (req,res)=>{
+    try{
+        const products = await Product.find().exec();
+        res.render('index',{products:products})
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 
-router.post('/insert',async (req,res)=>{
+router.get('/add-product',(req,res)=>{
+    if(req.session.login){
+        res.render('form')
+    }
+    else{
+        res.render('admin')
+    }
+})
+
+router.get('/manage',async (req,res)=>{
+    if(req.session.login){
+        try{
+            const products = await Product.find().exec();
+            res.render('manage',{products:products})
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    else{
+        res.render('admin')
+    }
+})
+router.get("/logout",(req,res)=>{
+    req.session.destroy()
+    res.redirect("/manage")
+})
+router.get('/delete/:id',async (req,res)=>{
+    try{
+        await Product.findByIdAndDelete(req.params.id,{
+            useFindAndModify:false
+        }).exec();
+        res.redirect('/manage')
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+router.get('/:id',async (req,res)=>{
+    try{
+        const product = await Product.findOne({_id:req.params.id}).exec();
+        res.render('product',{product:product})
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+
+router.post('/insert',upload.single("image"),async (req,res)=>{
     try {
         let data = new Product({
             name: req.body.name,
             price: req.body.price,
-            image: req.body.image,
+            image: req.file.filename,
             description: req.body.description,
         });
         await Product.saveProduct(data);
@@ -37,5 +93,50 @@ router.post('/insert',async (req,res)=>{
     }
 
 })
+
+router.post('/update',async (req,res)=>{
+    try {
+        const update_id = req.body.update_id
+        let data = {
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+        };
+        await Product.findByIdAndUpdate(update_id,data,{useFindAndModify:false}).exec()
+        res.redirect("/manage")
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+router.post('/edit',async (req,res)=>{
+    try {
+        const edit_id = req.body.edit_id
+        const product = await Product.findOne({_id:edit_id}).exec();
+        //นำข้อมูลเดิมไปเเสดงผล
+        res.render('edit',{product:product})
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+router.post("/login",(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+    const timeExpire = 30000 // 10 s
+    if(username === "admin" && password === "123"){
+        //create session
+        req.session.username = username
+        req.session.password = password
+        req.session.login = true
+        req.session.cookie.maxAge = timeExpire
+        res.redirect('/manage')
+    }else{
+        res.render('404')
+    }
+})
+
+
 
 module.exports = router;
